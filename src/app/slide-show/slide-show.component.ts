84b2dcfee246e1c5fac3809d3812183a1b69e76b
vmhau92 <vmhau92@gmail.com>
2022-01-08 17:48:47 +0700

    Add slideshow using carousel

--- src/app/slide-show/slide-show.component.ts
+++ src/app/slide-show/slide-show.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+// import { MessageService } from './message.service';
 
 @Component({
   selector: 'app-slide-show',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideShowComponent implements OnInit {
   
-  constructor() { }
+  constructor(private httpClient: HttpClient, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.getImage();
+    console.log('init');
+    
+    
+  }
+  listImages: any[] 
+  getImage() {
+    console.log("Enter get image");
+    this.httpClient.get('http://127.0.0.1:8081/listImage' ,{ responseType: 'json' })
+      .subscribe(
+        (res:Response) => {
+          if(res == null){
+            console.log("Res is null");
+          }
+          
+           this.listImages = Array.of(res);
+          //this.listImages = res.json().results;
+          console.log("Received" + this.listImages);
+        }
+      );
   }
 
+  sanitizeImageUrl(imageUrl: string): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+}
 }
